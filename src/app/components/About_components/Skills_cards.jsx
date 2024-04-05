821d037ca0583588ba92ca3307d2fdd2badf9835
noureddinecharifi <charifir80@gmail.com>
2024-04-05 02:20:13 +0000

    Images styling

--- src/app/components/About_components/Skills_cards.jsx
+++ src/app/components/About_components/Skills_cards.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function SkillsCard({ description, title, icon, color, iconColor}) {
     return (
-        <div className={`h-full p-5 flex gap-4 justify-start items-center rounded-md ${color}`}>
+        <div className={`h-full p-5 flex gap-4 justify-start items-center rounded-md ${color} shadow-lg overflow-hidden`}>
             <i className={`${icon} ${iconColor}`}></i>
 
             <div>
                 <h4 className='text-solid font-semibold text-sm mb-2'>{title}</h4>
-                <p className='text-solid text-[0.8em] w-[250px] text-justify font-normal'>
+                <p className='text-solid text-[0.8em] w-[220] font-normal'>
                     {description}
                 </p>
             </div>
